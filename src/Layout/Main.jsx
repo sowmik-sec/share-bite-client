2df0e8d398b2da4ef866ee5752eedcfc522bfc97
sowmik-sec <sowmik.sec@gmail.com>
2024-08-16 21:42:25 +0600

    create main layout

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar/Navbar";
 
 function Main() {
   return (
     <div>
+      <Navbar />
       <Outlet />
     </div>
   );
