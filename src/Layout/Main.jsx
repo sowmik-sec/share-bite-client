45ce5681d5a0de5ead1bc955473a3bb6d57da5fb
sowmik-sec <sowmik.sec@gmail.com>
2024-08-18 19:10:02 +0600

    create footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
+import Footer from "../shared/Footer/Footer";
 
 function Main() {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
