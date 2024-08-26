183ed60b8735aac45e287edee5de5abfbdc5955b
sowmik-sec <sowmik.sec@gmail.com>
2024-08-26 12:57:58 +0600

    show featured foods in home page

---
+++ src/pages/Home/FeaturedFood/FeaturedFood.jsx
@@ -0,0 +1,7 @@
+import ShowFoods from "../../ShowFoods/ShowFoods";
+
+function FeaturedFood() {
+  return <ShowFoods from={"featuredFood"} />;
+}
+
+export default FeaturedFood;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,7 +1,10 @@
+import FeaturedFood from "../FeaturedFood/FeaturedFood";
+
 function Home() {
   return (
     <div>
       <h2>Hello world</h2>
+      <FeaturedFood />
     </div>
   );
 }
